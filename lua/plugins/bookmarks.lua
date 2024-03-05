89bb5222c266f6581c0893da56fb9b7e22b621bc
fullmoon <1317812522@qq.com>
2024-03-05 10:08:21 +0800

    Add telescope-vim-bookmarks plugin

--- lua/plugins/bookmarks.lua
+++ lua/plugins/bookmarks.lua
@@ -2,5 +2,6 @@ return {
   'MattesGroeger/vim-bookmarks',
   cond = not vim.g.vscode,
   config = function()
+    vim.g.bookmark_sign = '♥'
   end
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,11 +9,14 @@ return {
     local telescope = require('telescope')
     local builtin = require('telescope.builtin')
 
+    telescope.load_extension('vim_bookmarks')
+
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>bm', ':Telescope vim_bookmarks all<CR>', {})
 
     telescope.setup({
       defaults = {
